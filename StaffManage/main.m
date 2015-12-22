7e198a7778ec51d47058304788c8d1753e35b92f
ytf2y <178884773@qq.com>
2015-12-22 10:12:35 +0800

    Initial Commit

---
+++ StaffManage/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StaffManage
+//
+//  Created by YanTianFeng on 15-12-22.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
